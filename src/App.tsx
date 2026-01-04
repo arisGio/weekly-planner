6ebc1421fc4ef2052326b58beaba574e6feee62e
Aris Giotis <apjoti@gmail.com>
2026-01-04 17:21:39 +0200

    create utils dir

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { PDFViewer } from "@react-pdf/renderer";
 import WeeklyPlanner from "./components/WeeklyPlanner/WeeklyPlanner";
-
-const creationDate = new Date(2026, 1, 4);
-const modificationDate = new Date(); // not working
+import { getCreationDate, getModificationDate } from "./utils/dates";
 
 const App = () => (
   <PDFViewer style={{ width: "100vw", height: "100vh" }}>
     <WeeklyPlanner
       title="Weekly Planner"
       author="Aris Giotis"
-      creationDate={creationDate}
-      modificationDate={modificationDate}
+      creationDate={getCreationDate()}
+      modificationDate={getModificationDate()}
     />
   </PDFViewer>
 );
---
+++ src/utils/dates.ts
@@ -0,0 +1,2 @@
+export const getCreationDate = () => new Date(2026, 1, 4);
+export const getModificationDate = () => new Date(); // not working
